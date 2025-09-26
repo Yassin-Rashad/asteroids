bf09da1d9a98f23b43c6695a8ae66988ef54fd68
Yassin-Rashad <yassinrashad4@gmail.com>
2025-09-26 22:10:00 +0300

    Moving Around

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from player import *
 
 
 def main():
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     pygame.init()
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -15,6 +15,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000 
